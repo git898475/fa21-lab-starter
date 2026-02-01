7fd823b3df2ece105a19d5b7eca3d28fbffe2733
git898475 <s64824910@gmail.com>
2026-02-01 10:43:50 +0800

    nothing changed

--- lab00/code.py
+++ lab00/code.py
@@ -7,9 +7,9 @@ def get_airspeed_velocity_of(unladen_swallow):
 def fizzbuzz(num):
   if str(num) in ["15"]:
     print(f"{num}: fizzbuzz")
-  elif str(num) in ["3", "6", "9", "12", "15", "18"]:
+  elif str(num) in ["3", "6", "9", "12", "18"]:
     print(f"{num}: fizz")
-  elif str(num) in ["5", "10", "15"]:
+  elif str(num) in ["5", "10"]:
     print(f"{num}: buzz")
 
 for i in range(1, 20):
